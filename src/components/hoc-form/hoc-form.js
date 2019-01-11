19ddee9283e4fed68bfc36818224680efd3ff1a7
翟林 <18731810122@163.com>
2019-01-11 17:24:02 +0800

    封装高阶组件

---
+++ src/components/hoc-form/hoc-form.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function HocForm(Com) {
+  class FormComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {}
+      this.handleChange = this.handleChange.bind(this)
+    }
+    handleChange = (key, value) => {
+      this.setState({
+        [key]: value
+      })
+    }
+    render() {
+      return (
+        <Com handleChange={this.handleChange} state={this.state} {...this.props}></Com>
+      )
+    }
+  }
+  return FormComponent
+}
